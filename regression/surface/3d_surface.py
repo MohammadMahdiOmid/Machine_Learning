b13155053223494f5c48af239ef5f5f61e46d008
MohammadMehdiOmid <MohammadMehdiOmid@gmail.com>
2021-02-23 09:48:48 +0330

    the packages of data and surface is created and codes of surface is here just for see more information about 3D

---
+++ regression/surface/3d_surface.py
@@ -0,0 +1,41 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+ #create x and y between -10 and 10 with the defference of 0.5
+x=np.arange(-10,10.1,0.5)
+y=np.arange(-10,10.1,0.5)
+
+print(x.shape)
+print(y.shape)
+
+print(x)
+print(y)
+
+# for changing 1D array to 2D array
+X,Y=np.meshgrid(x,y)
+
+#you can see it :)
+print(x.shape)
+print(y.shape)
+
+
+Z = X ** 2 + Y ** 2
+
+
+
+fig = plt.figure(figsize=(12, 8))
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X, Y, Z, cmap=plt.cm.rainbow)
+fig.colorbar(surf, shrink=0.5, aspect=5)
+plt.show()
+
+
+fig = plt.figure(figsize=(12, 8))
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X, Y, Z, cmap=plt.cm.rainbow)
+cset = ax.contour(X, Y, Z, zdir='z', offset=0, cmap=plt.cm.rainbow)
+fig.colorbar(surf, shrink=0.5, aspect=5)
+plt.show()
