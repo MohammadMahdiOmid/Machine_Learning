90df907863ecf28827793d678c85c6dd98695f7c
MohammadMehdiOmid <MohammadMehdiOmid@gmail.com>
2021-04-24 11:09:12 +0430

    SkLearn Linear Regression is added and all methods for implementatio of this project are created

---
+++ Supervised learning/linear regression/sklearn_linear_regression_sv/linear_regression.py
@@ -0,0 +1,21 @@
+from sklearn import preprocessing,svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+def read_database():
+    pass
+
+
+def creating_model():
+    pass
+
+def plot():
+    pass
+
+
+if __name__ == '__main__':
+    pass
