4233f01f94737cadbb9213636162b975ff443515
MohammadMahdiOmid <mohammadmehdiomid@gmail.com>
2022-09-20 20:06:26 +0430

    hypothesis function added

--- Machine_learning2022/Supervised_learning/linear_regression/multiple_linear_regression.py
+++ Machine_learning2022/Supervised_learning/linear_regression/multiple_linear_regression.py
@@ -1,8 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
 
 class MultiplrRegression:
-    def __init__(self,data):
+    def __init__(self, data):
         # extracting data
-        self.x=data[:,0]
-        self.y=data[:,1]
+        self.data = data
+        self.x = data[['Volume', 'Weight']]
+        self.y = data['CO2']
+        self.print(self.x)
+    def print(self,x):
+        print(x['Volume'])
+        print(x['Weight'])
+    def hypothesis(self, theta_0, theta_1, theta_2):
+        return theta_0 + theta_1 * self.x['Volume'] + theta_2 * self.x['Weight']
+
+
+if __name__ == '__main__':
+    data = pd.read_csv("data/data.csv")
+    new_object = MultiplrRegression(data)
