bc18a27d50bb700f1191efaefb6217da23ce7b03
MohammadMahdiOmid <mohammadmehdiomid@gmail.com>
2022-09-20 20:14:19 +0430

    Cost Function added

--- Machine_learning2022/Supervised_learning/linear_regression/multiple_linear_regression.py
+++ Machine_learning2022/Supervised_learning/linear_regression/multiple_linear_regression.py
@@ -10,11 +10,23 @@ class MultiplrRegression:
         self.x = data[['Volume', 'Weight']]
         self.y = data['CO2']
         self.print(self.x)
-    def print(self,x):
+
+    def print(self, x):
         print(x['Volume'])
         print(x['Weight'])
+
+    # Hypothesis Function With 2 variable
     def hypothesis(self, theta_0, theta_1, theta_2):
         return theta_0 + theta_1 * self.x['Volume'] + theta_2 * self.x['Weight']
+
+    # Cost Function
+    def cost_function(self, theta_0, theta_1, theta_2):
+        # To Demonstrate Loss Function
+        cost = []
+        # To finding best parameters
+        j_theta = 0.5 * (self.hypothesis(theta_0, theta_1, theta_2) ** 2).mean()
+        return j_theta
+
 
 
 if __name__ == '__main__':
