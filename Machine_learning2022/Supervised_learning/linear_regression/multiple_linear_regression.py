5ff7722856fe37efdfdcca7bb59ed6f4447a6c75
MohammadMahdiOmid <mohammadmehdiomid@gmail.com>
2022-09-20 20:17:41 +0430

    Gradient Descent added and Random theta to start added

--- Machine_learning2022/Supervised_learning/linear_regression/multiple_linear_regression.py
+++ Machine_learning2022/Supervised_learning/linear_regression/multiple_linear_regression.py
@@ -10,6 +10,7 @@ class MultiplrRegression:
         self.x = data[['Volume', 'Weight']]
         self.y = data['CO2']
         self.print(self.x)
+        self.gradient_descent()
 
     def print(self, x):
         print(x['Volume'])
@@ -27,7 +28,14 @@ class MultiplrRegression:
         j_theta = 0.5 * (self.hypothesis(theta_0, theta_1, theta_2) ** 2).mean()
         return j_theta
 
-
+    def gradient_descent(self):
+        # Random theta to start
+        theta_0=np.random.randn()
+        theta_1=np.random.randn()
+        theta_2=np.random.randn()
+        print("Begining theta_0 is:",theta_0)
+        print("Begining theta_1 is:",theta_1)
+        print("Begining theta_2 is:",theta_2)
 
 if __name__ == '__main__':
     data = pd.read_csv("data/data.csv")
